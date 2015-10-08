099e667c43aa9d6ad9d952d4ea621494d6527510
jackson_gross@hotmail.com <jgross@acresta.com>
2015-10-08 18:39:51 +1100

    Fixed app titles and sample shellfile

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var file = 'shellfile.json';
 jsonfile.readFile(file, function(err, config) {
     if (err) throw "Couldn't find shellfile.json. Please make sure it is in the current working directory.";
 
-    shell.exec('cordova create ' + config.name + ' ' + config.identifier + ' ' + config.displayName || config.name);
+    shell.exec('cordova create ' + config.name + ' ' + config.identifier + ' "' + config.displayName || config.name + '"');
 
     shell.exec('rm -rf ' + config.name+ '/www');
     shell.exec('node $(npm config get prefix)/lib/node_modules/ac-appshell-generator/node_modules/@blinkmobile/appcache-fetcher/index.js ' + config.answerspace + ' ' + config.name + '/www');
